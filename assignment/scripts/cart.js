4462889a78165b9750df7cf4ecb6446044621b52
Carlos Kelley <carloskelley@protonmail.com>
2022-04-05 15:35:27 -0500

    basic complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,30 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item){
+    basket.push(item);
+    if(basket.includes(item)){
+        return true;
+    }
+    else return false;
+}
+console.log('Basket is', basket);
+console.log('Adding eggs and milk (expect true)', addItem('eggs'), addItem('milk'));
+console.log('Basket is now', basket);
+
+function listItems(){
+    console.log('Items are:')
+    for (let i=0; i<basket.length;i++){
+        console.log(basket[i], '\n');
+    }
+}
+listItems();
+
+function empty(){
+    basket = [];
+    }
+    
+    empty();
+    console.log('Basket emptied. Basket is now', basket);
